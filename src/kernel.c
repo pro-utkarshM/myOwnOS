7a7d967aa2dad5eb4589aaf59d825743ac306434
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2025-04-30 18:34:04 +0530

    feat: cooked block-based_heap

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "idt/idt.h"
+#include "memory/heap/kheap.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -74,6 +75,9 @@ void kernel_main()
     terminal_initialize();
     print("Hello world!\ntest");
 
+    // Initialize the heap
+    kheap_init();
+    
     // Initialize the interrupt descriptor table
     idt_init();
     
